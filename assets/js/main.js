118755b0cf340fd93bc235a64d838a379a4d1ef6
thanhbui <buiminhthanh152@gmail.com>
2023-08-19 11:32:43 +0700

    new skill update 19/8

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,3 +233,33 @@
     document.contact-form.submit();
     document.contact-form.reset();
     }
+
+/**
+  Tab UI
+ * ** */
+  var tabLinks = document.querySelectorAll(".tablinks");
+  var tabContent =document.querySelectorAll(".tabcontent");
+  
+  tabLinks.forEach(function(el) {
+     el.addEventListener("click", openTabs);
+  });
+  
+  
+  function openTabs(el) {
+     var btn = el.currentTarget; // lắng nghe sự kiện và hiển thị các element
+     var electronic = btn.dataset.electronic; // lấy giá trị trong data-electronic
+   
+     tabContent.forEach(function(el) {
+        el.classList.remove("active");
+     }); //lặp qua các tab content để remove class active
+  
+     tabLinks.forEach(function(el) {
+        el.classList.remove("active");
+     }); //lặp qua các tab links để remove class active
+  
+     document.querySelector("#" + electronic).classList.add("active");
+     // trả về phần tử đầu tiên có id="" được add class active
+     
+     btn.classList.add("active");
+     // các button mà chúng ta click vào sẽ được add class active
+  }
